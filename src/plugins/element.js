12415cd5345f5b83c38f7bc94f4528c9a7409423
kevinxiaoge <kevinxiaoge@qq.com>
2020-05-30 02:50:17 +0800

    解决语法的问题

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
-import { Button,Form,FormItem,Input } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input
+} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
